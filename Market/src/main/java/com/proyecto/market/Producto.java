8124861fb37eae2cedf29675439549acb66f4937
jdAgudelo0 <166195651+jdAgudelo0@users.noreply.github.com>
2024-09-28 20:26:16 -0500

    Nuevooo cambiooo somo flash
    carpeta para los enums y clase producto

---
+++ Market/src/main/java/com/proyecto/market/Producto.java
@@ -0,0 +1,84 @@
+package com.proyecto.market;
+import com.proyecto.market.Enum.Estado;
+import java.util.ArrayList;
+
+public class Producto {
+    private String nombreProducto;
+    private String codigo;
+    private String imagen;
+    private String categoria;
+    private float precio;
+    private Estado estado;
+    private ArrayList<Comentario> comentarios;
+    private int likes;
+
+    public Producto(String nombreProducto, String codigo, String imagen, String categoria, float precio, Estado estado) {
+
+        this.nombreProducto= nombreProducto;
+        this.codigo = codigo;
+        this.imagen = imagen;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.estado = estado;
+        this.comentarios = new ArrayList<Comentario>();
+        this.likes = 0;
+    }
+
+    public Producto() {}
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public ArrayList<Comentario> getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(ArrayList<Comentario> comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+}
