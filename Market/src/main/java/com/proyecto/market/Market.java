2ab4ae6a45926103b5c46016c58b7b979bf5162d
StefaniaH016 <stefaniaj.herreraa@uqvirtual.edu.co>
2024-09-28 21:57:17 -0500

    Update Market.java
    Implementacion del singleton a la clase Market y sus respectivos metodos

--- Market/src/main/java/com/proyecto/market/Market.java
+++ Market/src/main/java/com/proyecto/market/Market.java
@@ -1,4 +1,32 @@
 package com.proyecto.market;
 
 public class Market {
+    private Vendedor vendedores;
+    private static Market market;
+
+    //singleton para Market
+    public static Market getInstance(){
+
+        if(market == null){
+            market = new Market();
+        }
+        return market;
+    }
+
+    public void registrarUsuario(String nombre, String apellidos, String cedula, String direccion, String usuario, String contraseña){
+
+
+    }
+
+    public void verificarDatos(){
+
+    }
+
+    public void login(String Usuario, String contraseña){
+
+    }
+
+    public void sugerir contacto(Vendedor vendedores){
+        
+    }
 }
