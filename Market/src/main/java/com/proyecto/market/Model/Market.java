e3337ddda0d327cf88ca795916cc5478277a3825
Elkinlol <168575037+Elkinlol@users.noreply.github.com>
2024-10-18 22:37:19 -0500

    Commit Elkin

--- Market/src/main/java/com/proyecto/market/Model/Market.java
+++ Market/src/main/java/com/proyecto/market/Model/Market.java
@@ -1,8 +1,9 @@
 package com.proyecto.market.Model;
 
 public class Market {
-    private Vendedor vendedores;
+    private ArrayList<Vendedor> vendedores;
     private static Market market;
+    private ArrayList<Producto> productos
 
     //singleton para Market
     public static Market getInstance(){
