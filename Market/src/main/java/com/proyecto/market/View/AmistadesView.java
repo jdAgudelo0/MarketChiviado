fcda0ee9961d1d970651c8d04bf3db9838ba1f82
jdAgudelo0 <166195651+jdAgudelo0@users.noreply.github.com>
2024-11-17 18:01:04 -0500

    njkb

--- Market/src/main/java/com/proyecto/market/View/AmistadesView.java
+++ Market/src/main/java/com/proyecto/market/View/AmistadesView.java
@@ -1,4 +1,46 @@
 package com.proyecto.market.View;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AmistadesView {
+
+    @FXML
+    private Button btnVolver;
+
+    @FXML
+    private ListView<?> solicitudesList;
+
+    @FXML
+    private ListView<?> vendedoresAliadosList;
+
+    @FXML
+    void volver(ActionEvent actionEvent) {
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        currentStage.close();
+
+        //Intenta abrir el inicio sesion
+        try{
+            FXMLLoader loader= new FXMLLoader(getClass().getResource("/com/proyecto/market/muro.fxml"));
+            Parent root = loader.load();
+            Stage stage= new Stage();
+            stage.setTitle("Inicio sesion");
+            stage.setScene(new Scene(root));
+            stage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+
+        }
+
+    }
+
 }
--- Market/src/main/java/com/proyecto/market/View/SugerirAmistadesController.java
+++ Market/src/main/java/com/proyecto/market/View/SugerirAmistadesController.java
@@ -1,4 +1,57 @@
 package com.proyecto.market.View;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class SugerirAmistadesController {
+
+    @FXML
+    private Button btnVolver;
+
+    @FXML
+    private ImageView logoImageView;
+
+    @FXML
+    private ImageView personImageView1;
+
+    @FXML
+    private ImageView personImageView2;
+
+    @FXML
+    private ImageView personImageView3;
+
+    @FXML
+    private ImageView personImageView4;
+
+    @FXML
+    void volver(ActionEvent actionEvent) {
+        //Cierra la ventana actual
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        currentStage.close();
+
+        //Intenta abrir el inicio sesion
+        try{
+            FXMLLoader loader= new FXMLLoader(getClass().getResource("/com/proyecto/market/muro.fxml"));
+            Parent root = loader.load();
+            Stage stage= new Stage();
+            stage.setTitle("Inicio sesion");
+            stage.setScene(new Scene(root));
+            stage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+
+        }
+    }
+
+
 }
+
